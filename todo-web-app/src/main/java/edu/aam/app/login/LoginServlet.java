5098f7fe1e790ca22cf0e90ea2521f476f025fd4
Arif Mondal <arifali.mondal@gmail.com>
2019-10-10 23:27:45 +0300

    Login servlet added
    pom, and web xml added and login servlet add

---
+++ todo-web-app/src/main/java/edu/aam/app/login/LoginServlet.java
@@ -0,0 +1,26 @@
+package edu.aam.app.login;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		
+		request.setAttribute("name", request.getParameter("name"));
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+	}
+
+}
---
+++ todo-web-app/src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Yahoo!!! from JSP</title>
+	</head>
+	<body>
+		My First JSP!!! My name is ${name}
+	</body>
+</html>
