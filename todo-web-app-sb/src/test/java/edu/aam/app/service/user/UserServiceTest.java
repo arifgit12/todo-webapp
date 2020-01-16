a9db8657f0300a7be2115a899af05d7422f0c74a
Arif A Mondal <arifali.mondal@gmail.com>
2020-01-16 14:37:33 +0300

    updates UserService junit test

--- todo-web-app-sb/src/test/java/edu/aam/app/service/user/UserServiceTest.java
+++ todo-web-app-sb/src/test/java/edu/aam/app/service/user/UserServiceTest.java
@@ -1,13 +1,27 @@
 package edu.aam.app.service.user;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
 
+import edu.aam.app.model.User;
 import edu.aam.app.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
 
     @Mock
     private UserRepository userRepository;
@@ -15,12 +29,32 @@ public class UserServiceTest {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Before
-    public void frameUp() {
+    public void init() {
+        MockitoAnnotations.initMocks(this);
         bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    }
+
+    private User setUser() {
+        User user = new User();
+        user.setFirstName("test");
+        user.setLastName("test");
+        user.setEmail("testmail@gmail.com");
+        return user;
+    }
+
+    @Test
+    public void findUserByEmailNullTest() {
+        when(userRepository.findByEmail("test1@gmail.com")).thenReturn(null);
+        User testuser = userService.findUserByEmail("test1@gmail.com");
+        assertNull(testuser);
     }
 
     @Test
     public void findUserByEmailTest() {
-        System.out.println("welcome to test demo");
+        User user = setUser();
+        when(userRepository.findByEmail("test@gmail.com")).thenReturn(user);
+        User testuser = userService.findUserByEmail("test@gmail.com");
+        assertNotNull(testuser);
+        assertEquals("testmail@gmail.com", testuser.getEmail());
     }
 }
