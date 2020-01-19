0255d8b81e47aa14478602f7e81584ffeb75f611
Arif A Mondal <arifali.mondal@gmail.com>
2020-01-19 09:41:46 +0300

    add TodoService test

---
+++ todo-web-app-sb/src/test/java/edu/aam/app/service/todo/TodoServiceTest.java
@@ -0,0 +1,18 @@
+package edu.aam.app.service.todo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.reflect.Whitebox;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TodoServiceTest {
+
+}
