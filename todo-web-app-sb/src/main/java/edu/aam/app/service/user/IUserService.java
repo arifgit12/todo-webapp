333857721653b70739c9d4c27831b84a79979e0f
Arif A Mondal <arifali.mondal@gmail.com>
2020-04-14 10:05:36 +0300

    add UserService interface

---
+++ todo-web-app-sb/src/main/java/edu/aam/app/service/user/IUserService.java
@@ -0,0 +1,17 @@
+package edu.aam.app.service.user;
+
+import edu.aam.app.model.Task;
+import edu.aam.app.model.Todo;
+import edu.aam.app.model.User;
+
+import java.util.List;
+
+public interface IUserService {
+    User findUserByEmail(String email);
+    List<Task> findTaskByUserEmail(String email);
+    List<Todo> getTodoByUserName(String userName);
+    Todo getTodoByUserName(String userName, Long todoId);
+    User createUser(UserDTO userDTO);
+    void updatePassword(String email, String newPassword);
+    UserDTO convertUserDto(User user);
+}
