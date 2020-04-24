a2cd7a0d0835a528857342572edd6e8f83cbf1c9
Arif A Mondal <arifali.mondal@gmail.com>
2020-04-25 02:22:28 +0300

    update AuthenticatedUser

--- todo-web-app-sb/src/main/java/edu/aam/app/util/AuthenticatedUser.java
+++ todo-web-app-sb/src/main/java/edu/aam/app/util/AuthenticatedUser.java
@@ -3,7 +3,6 @@ package edu.aam.app.util;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public class AuthenticatedUser {
 
