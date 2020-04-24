078630794b75dcc39a992f1731debbbf5a26e046
Arif A Mondal <arifali.mondal@gmail.com>
2020-04-25 02:22:08 +0300

    update UserService

--- todo-web-app-sb/src/main/java/edu/aam/app/service/user/UserService.java
+++ todo-web-app-sb/src/main/java/edu/aam/app/service/user/UserService.java
@@ -12,9 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.*;
 
 @Service
+@Transactional
 public class UserService implements IUserService {
 
     private static final Logger log = LoggerFactory.getLogger(UserService.class);
