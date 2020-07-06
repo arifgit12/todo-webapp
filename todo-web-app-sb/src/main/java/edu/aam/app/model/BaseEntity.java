58e06f6d500c3bf8c4da22d03e4bc186ca7bb526
Arif A Mondal <arifali.mondal@gmail.com>
2020-07-06 22:25:46 +0300

    add BaseEntity

---
+++ todo-web-app-sb/src/main/java/edu/aam/app/model/BaseEntity.java
@@ -0,0 +1,61 @@
+package edu.aam.app.model;
+
+import edu.aam.app.util.DateUtils;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+public class BaseEntity {
+    /**
+     * Create time.
+     */
+    @Column(name = "create_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+
+    /**
+     * Update time.
+     */
+    @Column(name = "update_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTime;
+
+    @PrePersist
+    protected void prePersist() {
+        Date now = DateUtils.now();
+        if (createTime == null) {
+            createTime = now;
+        }
+
+        if (updateTime == null) {
+            updateTime = now;
+        }
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        updateTime = new Date();
+    }
+
+    @PreRemove
+    protected void preRemove() {
+        updateTime = new Date();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
