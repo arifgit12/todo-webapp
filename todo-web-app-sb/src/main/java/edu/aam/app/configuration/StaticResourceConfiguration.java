d0961c3ea020acdc77ed2b7d3806babe9b365e1c
Arif A Mondal <arifali.mondal@gmail.com>
2020-07-09 13:06:41 +0300

    add resource and resource configuration

---
+++ todo-web-app-sb/src/main/java/edu/aam/app/configuration/StaticResourceConfiguration.java
@@ -0,0 +1,14 @@
+package edu.aam.app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
