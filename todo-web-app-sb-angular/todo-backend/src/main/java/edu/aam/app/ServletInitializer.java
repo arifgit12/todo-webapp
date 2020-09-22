fa648ebee68a7361ff1cc57991c30d5d2c35acac
Arif A Mondal <arifali.mondal@gmail.com>
2020-09-22 22:07:13 +0300

    add todo-backend service

---
+++ todo-web-app-sb-angular/todo-backend/src/main/java/edu/aam/app/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.aam.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TodoBackendApplication.class);
+	}
+
+}
