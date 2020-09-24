429d603cb227c671bfa85fa598b2be59e10c8589
Arif A Mondal <arifali.mondal@gmail.com>
2020-09-24 21:57:59 +0300

    FE - add todo

--- todo-web-app-sb-angular/todo-frontend/src/app/todo/todo.component.ts
+++ todo-web-app-sb-angular/todo-frontend/src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Todo } from '../list-todos/list-todos.component';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  todo:Todo;
+
+  constructor(
+    private todoService: TodoDataService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.todo = new Todo(this.id,'', false, new Date());
+
+    if(this.id != -1) {
+      this.todoService.retrieveTodo('arifmondal', this.id)
+        .subscribe(
+          (response) => {
+            this.todo = response;
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
+    }
   }
 
+  saveTodo() {
+    console.log(this.todo);
+    if(this.id === -1) {
+      // create todo
+      this.todoService.createTodo('arifmondal', this.todo)
+        .subscribe(
+          (response) => {
+            console.log(response);
+            this.router.navigate(['todos']);
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
+    } else {
+      // update todo
+      this.todoService.updateTodo('arifmondal', this.id, this.todo)
+        .subscribe(
+          (response) => {
+            console.log(response);
+            this.router.navigate(['todos']);
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
+    }
+  }
 }
