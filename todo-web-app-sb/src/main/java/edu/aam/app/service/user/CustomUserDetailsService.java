edce5af570c1a0020a063bbf5c97ae645b382cbd
Arif A Mondal <arifali.mondal@gmail.com>
2020-11-23 21:16:30 +0300

    spring-security-block-brute-force-authentication-attempts

--- todo-web-app-sb/src/main/java/edu/aam/app/service/user/CustomUserDetailsService.java
+++ todo-web-app-sb/src/main/java/edu/aam/app/service/user/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package edu.aam.app.service.user;
 import edu.aam.app.model.User;
 import edu.aam.app.model.Role;
 import edu.aam.app.repository.UserRepository;
+import edu.aam.app.security.LoginAttemptService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,9 +28,22 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    @Autowired
+    private HttpServletRequest request;
+
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        String ip = getClientIP();
+
+        if (loginAttemptService.isBlocked(ip)) {
+            System.out.println(ip);
+            throw new UsernameNotFoundException("User '" + ip + "' blocker");
+        }
 
         User user = userRepository.findByEmail(email);
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
@@ -41,5 +57,13 @@ public class CustomUserDetailsService implements UserDetailsService {
         } else {
             throw new UsernameNotFoundException(email);
         }
+    }
+
+    private String getClientIP() {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null){
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
     }
 }
